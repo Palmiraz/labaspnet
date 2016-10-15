6a5e9160dd886a80f578928632dca0d9eab83ee5
marco <markorex7@gmail.com>
2016-10-14 23:52:39 -0500

    Se integran ejercicios para almacenar un cliente

--- ShoeEcommers.LogicLayer/Repositories/CustomerRepository.cs
+++ ShoeEcommers.LogicLayer/Repositories/CustomerRepository.cs
@@ -1,29 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using ShoeEcommers.LogicLayer.DataObjects;
 using ShoeEcommers.LogicLayer.Modelos;
 
 namespace ShoeEcommers.LogicLayer.Repositories
 {
-    public class CustomerRepository
+    public class CustomerRepository : IDisposable
     {
+        private readonly SqlConnection _cn;
+        public CustomerRepository()
+        {
+            _cn = ConnectionProvider.GetConnection();
+        }
         public List<Customers> GetCustomers(string name = "")
         {
-            List<Customers> result = new List<Customers>();
-            for(int i = 1; i < 10; i++)
-            {
-                Customers customer = new Customers();
-                customer.Id = i;
-                customer.DateCreated = DateTime.Now;
-                customer.Name = "name" + i;
-                customer.FirstName = "app" + i;
-                customer.LastName = "appMat" + i;
-                customer.DateBirth = DateTime.Now.AddYears(-18);
-                result.Add(customer);
-            }
-            return result;
+            //TODO: Consumir el store para obtener la lista de usuario
+            return null;
         }
 
+        public Customers GetCustomerById(int id)
+        {
+            //TODO: Obtener un cliente por id
+            return null;
+        }
+
+        public bool SaveCustomer(Customers customer)
+        {
+            //TODO:Guardar un cliente
+            return false;
+        }
+
+        public bool DeleteCustomer(int id)
+        {
+            //TODO: Borrar un cliente por id
+            return false;
+        }
+
+
+
+        private bool _disposed;
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+            Dispose(true);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!_disposed && disposing)
+            {
+                if (_cn.State == ConnectionState.Open)
+                {
+                    _cn.Close();
+                }
+                _cn.Dispose();
+                _disposed = true;
+            }
+        }
     }
 }
