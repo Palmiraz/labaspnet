aa7d9c737a63212450af41bb20893072b3825181
marco <markorex7@gmail.com>
2016-10-15 06:04:44 -0500

    Se agrega la parte de logica de negocios

---
+++ ExamplesLinq/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExamplesLinq.Models;
+
+namespace ExamplesLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListData list = new ListData();
+            list.GetProductList();
+        }
+    }
+}
