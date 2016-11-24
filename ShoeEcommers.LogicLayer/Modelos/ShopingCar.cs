d26a13d4018667492433e5cad391b4c4e1673e29
marcoluna7 <markorex7@gmail.com>
2016-11-24 12:29:50 -0600

    Se agregan varios archivos

---
+++ ShoeEcommers.LogicLayer/Modelos/ShopingCar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoeEcommers.LogicLayer.Modelos
+{
+    public class ShopingCar
+    {
+        public int IdSku { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ ShoesEcommers.ShopWeb/Controllers/CheckoutController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShoeEcommers.LogicLayer.Modelos;
+using ShoeEcommers.LogicLayer.Entities;
+using ShoesEcommers.ShopWeb.Globals;
+
+namespace ShoesEcommers.ShopWeb.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly ecommersEntities _dc;
+        public CheckoutController()
+        {
+            _dc = new ecommersEntities();
+        }
+
+
+        public ActionResult Index()
+        {
+            ShopingCars car = Vars.CurrentCar;
+            car.SkusSelect.ForEach(se=>se.Sku = _dc.Skus.First(s=>s.Id == se.IdSku));
+            return View(car);
+        }
+
+        [Authorize]
+        public ActionResult PayProduct()
+        {
+            return View();
+        }
+
+
+
+    }
+}
